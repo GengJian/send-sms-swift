8ee7000610650f4de726c1357669418184c604a2
Soul <soulstayreal@gmail.com>
2021-12-16 13:19:36 +0800

    Initial Commit

---
+++ SendShoesSMS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SendShoesSMS
+//
+//  Created by Soul on 2021/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
