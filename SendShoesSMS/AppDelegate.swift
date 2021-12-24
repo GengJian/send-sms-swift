333b3c0a194da69935e43399f2f5348e9a2e8948
Soul <soulstayreal@gmail.com>
2021-12-24 15:43:28 +0800

    Dependency - 添加IQKeyboard(SPM)

--- SendShoesSMS/AppDelegate.swift
+++ SendShoesSMS/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // 启动键盘管理
+        IQKeyboardManager.shared.enable = true
+        
         return true
     }
 
